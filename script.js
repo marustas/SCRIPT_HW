09a57296266d1e25e93e37bd7bac0d4817f7fb30
Stanislau Marudau <marudov.stas@gmail.com>
2022-11-19 12:36:12 +0200

    Resistor color changing added

--- script.js
+++ script.js
@@ -1,6 +1,166 @@
-const btn = document.getElementById('btn');
+let blackButton1 = document.getElementById('black');
+let brownButton1 = document.getElementById('brown');
+let redButton1 = document.getElementById('red');
+let orangeButton1 = document.getElementById('orange');
+let yellowButton1 = document.getElementById('yellow');
+let greenButton1 = document.getElementById('green');
+let blueButton1 = document.getElementById('blue');
+let violetButton1 = document.getElementById('violet');
+let greyButton1 = document.getElementById('grey');
+let whiteButton1 = document.getElementById('white');
+let goldButton1 = document.getElementById('gold');
+let silverButton1 = document.getElementById('silver');
 
-btn.addEventListener('click', function onClick() {
-    btn.style.backgroundColor = 'salmon';
-    btn.style.color = 'white';
+let blackButton2 = document.getElementById('black2');
+let brownButton2 = document.getElementById('brown2');
+let redButton2 = document.getElementById('red2');
+let orangeButton2 = document.getElementById('orange2');
+let yellowButton2 = document.getElementById('yellow2');
+let greenButton2 = document.getElementById('green2');
+let blueButton2 = document.getElementById('blue2');
+let violetButton2 = document.getElementById('violet2');
+let greyButton2 = document.getElementById('grey2');
+let whiteButton2 = document.getElementById('white2');
+
+let blackButton3 = document.getElementById('black3');
+let brownButton3 = document.getElementById('brown3');
+let redButton3 = document.getElementById('red3');
+let orangeButton3 = document.getElementById('orange3');
+let yellowButton3 = document.getElementById('yellow3');
+let greenButton3 = document.getElementById('green3');
+let blueButton3 = document.getElementById('blue3');
+let violetButton3 = document.getElementById('violet3');
+let greyButton3 = document.getElementById('grey3');
+let whiteButton3 = document.getElementById('white3');
+//let goldButton2 = document.getElementById('gold2');
+//let silverButton2 = document.getElementById('silver2');
+
+
+let band1 = document.getElementById('band1');
+let band2 = document.getElementById('band2');
+let band3 = document.getElementById('band3');
+let band4 = document.getElementById('band4');
+
+
+// First band color regulation
+blackButton1.addEventListener('click', function onClick() {
+    band1.style.backgroundColor = 'black';
+});
+
+brownButton1.addEventListener('click', function onClick() {
+    band1.style.backgroundColor = 'brown';
+});
+
+redButton1.addEventListener('click', function onClick() {
+    band1.style.backgroundColor = 'red';
+});
+
+orangeButton1.addEventListener('click', function onClick() {
+    band1.style.backgroundColor = 'orange';
+});
+
+yellowButton1.addEventListener('click', function onClick() {
+    band1.style.backgroundColor = 'yellow';
+});
+
+greenButton1.addEventListener('click', function onClick() {
+    band1.style.backgroundColor = 'green';
+});
+
+blueButton1.addEventListener('click', function onClick() {
+    band1.style.backgroundColor = 'blue';
+});
+
+violetButton1.addEventListener('click', function onClick() {
+    band1.style.backgroundColor = 'violet';
+});
+
+greyButton1.addEventListener('click', function onClick() {
+    band1.style.backgroundColor = 'grey';
+});
+
+whiteButton1.addEventListener('click', function onClick() {
+    band1.style.backgroundColor = 'white';
+});
+
+// Second band color regulation
+blackButton2.addEventListener('click', function onClick() {
+    band2.style.backgroundColor = 'black';
+});
+
+brownButton2.addEventListener('click', function onClick() {
+    band2.style.backgroundColor = 'brown';
+});
+
+redButton2.addEventListener('click', function onClick() {
+    band2.style.backgroundColor = 'red';
+});
+
+orangeButton2.addEventListener('click', function onClick() {
+    band2.style.backgroundColor = 'orange';
+});
+
+yellowButton2.addEventListener('click', function onClick() {
+    band2.style.backgroundColor = 'yellow';
+});
+
+greenButton2.addEventListener('click', function onClick() {
+    band2.style.backgroundColor = 'green';
+});
+
+blueButton2.addEventListener('click', function onClick() {
+    band2.style.backgroundColor = 'blue';
+});
+
+violetButton2.addEventListener('click', function onClick() {
+    band2.style.backgroundColor = 'violet';
+});
+
+greyButton2.addEventListener('click', function onClick() {
+    band2.style.backgroundColor = 'grey';
+});
+
+whiteButton2.addEventListener('click', function onClick() {
+    band2.style.backgroundColor = 'white';
+});
+
+// Third band color regulation
+blackButton3.addEventListener('click', function onClick() {
+    band3.style.backgroundColor = 'black';
+});
+
+brownButton3.addEventListener('click', function onClick() {
+    band3.style.backgroundColor = 'brown';
+});
+
+redButton3.addEventListener('click', function onClick() {
+    band3.style.backgroundColor = 'red';
+});
+
+orangeButton3.addEventListener('click', function onClick() {
+    band3.style.backgroundColor = 'orange';
+});
+
+yellowButton3.addEventListener('click', function onClick() {
+    band3.style.backgroundColor = 'yellow';
+});
+
+greenButton3.addEventListener('click', function onClick() {
+    band3.style.backgroundColor = 'green';
+});
+
+blueButton3.addEventListener('click', function onClick() {
+    band3.style.backgroundColor = 'blue';
+});
+
+violetButton3.addEventListener('click', function onClick() {
+    band3.style.backgroundColor = 'violet';
+});
+
+greyButton3.addEventListener('click', function onClick() {
+    band3.style.backgroundColor = 'grey';
+});
+
+whiteButton3.addEventListener('click', function onClick() {
+    band3.style.backgroundColor = 'white';
 });
